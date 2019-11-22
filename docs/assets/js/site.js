dcbbeab4faffce34804ac4e07e6703954762eff8
Robert James Gabriel <robert_gabriel@outlook.com>
2019-11-21 20:38:58 -0600

    version 2.6.0

--- docs/assets/js/site.js
+++ docs/assets/js/site.js
@@ -18,6 +18,7 @@ setTimeout(() => {
     let checkoutButtonTwo = document.getElementById('checkout-button-1');
     let checkoutButtonThree = document.getElementById('checkout-button-plan_G1O8Rztm6fFf4R');
     let checkoutButtonFour = document.getElementById('checkout-button-plan_G1OnSt8DIms1PY');
+    let checkoutButtonStudent = document.getElementById('checkout-button-plan_GDtSwhVHQXfaSw');
 
 
     if (checkoutButtonOne !== null) {
@@ -127,6 +128,39 @@ setTimeout(() => {
             // them to Checkout.
             stripe.redirectToCheckout({
                 items: [{plan: 'plan_G1OnSt8DIms1PY', quantity: 1}],
+          
+                // Do not rely on the redirect to the successUrl for fulfilling
+                // purchases, customers may not always reach the success_url after
+                // a successful payment.
+                // Instead use one of the strategies described in
+                // https://stripe.com/docs/payments/checkout/fulfillment
+                successUrl: 'https://www.helperbird.com/success',
+                cancelUrl: 'https://www.helperbird.com/cancel',
+              })
+              .then(function (result) {
+                if (result.error) {
+                  // If `redirectToCheckout` fails due to a browser or network
+                  // error, display the localized error message to your customer.
+                  var displayError = document.getElementById('error-message');
+                  displayError.textContent = result.error.message;
+                }
+              });
+        });
+
+
+    }
+
+
+    if (checkoutButtonStudent !== null) {
+        checkoutButtonStudent.addEventListener('click', function () {
+            // When the customer clicks on the button, redirect
+            // them to Checkout.
+            // When the customer clicks on the button, redirect
+            // them to Checkout.
+            // When the customer clicks on the button, redirect
+            // them to Checkout.
+            stripe.redirectToCheckout({
+                items: [{plan: 'plan_GDtSwhVHQXfaSw', quantity: 1}],
           
                 // Do not rely on the redirect to the successUrl for fulfilling
                 // purchases, customers may not always reach the success_url after
