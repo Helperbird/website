eea6e21dde0cb57bf20ed7153de1e8bc0ff98ee8
Robert James Gabriel <robert_gabriel@outlook.com>
2020-03-29 05:52:20 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -684,7 +684,9 @@ module.exports = {
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
+
       importsDirectory: 'assets/',
+      exclude: [/cname|app.css|robots.txt|/],
       offlineGoogleAnalytics: true,
       clientsClaim: true,
       skipWaiting: true,
