3bb93bbc3c8b20fba0a621ef72d6f0e84dfcbf7d
Robert James Gabriel <robert_gabriel@outlook.com>
2020-06-16 15:14:03 +0100

    Added 25.0.0 release

--- webpack.config.js
+++ webpack.config.js
@@ -384,6 +384,20 @@ module.exports = {
 
 
     new HtmlWebpackPlugin({
+      filename: 'updates/25-0-0.html',
+      template: 'templates/updates/25-0-0.pug',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+    }),
+
+
+    new HtmlWebpackPlugin({
       filename: 'updates/24-1-0.html',
       template: 'templates/updates/24-1-0.pug',
       minify: {
