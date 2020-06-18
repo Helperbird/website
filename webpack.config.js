9f50d870d127d15aa44c9fd7d9a5f39bc32e1644
Robert James Gabriel <robert_gabriel@outlook.com>
2020-06-18 21:20:03 +0100

    Friends  page

--- webpack.config.js
+++ webpack.config.js
@@ -314,6 +314,20 @@ module.exports = {
         useShortDoctype: true
       },
     }),
+
+    new HtmlWebpackPlugin({
+      filename: 'friend/dai.html',
+      template: './templates/friend/dai.pug',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+    }),
+
     new HtmlWebpackPlugin({
       filename: 'features/ruler.html',
       template: './templates/features/ruler.pug',
