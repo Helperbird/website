786e8202dc4e595905dc2f2ae88d186b211abd81
Robert James Gabriel <robert_gabriel@outlook.com>
2020-10-10 14:18:15 +0100

    Updated nav

--- app/assets/js/core/nav.js
+++ app/assets/js/core/nav.js
@@ -25,8 +25,25 @@ function load() {
 
 }
 
+function dropdown(){
+   
+        document.getElementById("myDropdown").classList.toggle("show");
+
+      
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function(e) {
+        if (!e.target.matches('.dropbtn')) {
+        var myDropdown = document.getElementById("myDropdown");
+          if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+          }
+        }
+      }
+}
+
 
 
 export {
-    load
+    load,
+    dropdown
 }; // a list of exported variables
