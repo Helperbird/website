19a67e87628bd09069a4df2d9243ecc0214193cb
Robert James Gabriel <robert_gabriel@outlook.com>
2020-11-18 00:08:55 +0000

    Updated images

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,65 @@
 const image = require('gulp-image');
-const {
-  task,
-  src,
-  dest
-} = require('gulp');
+const { task, src, dest } = require('gulp');
 const html2pug = require('gulp-html2pug');
-var clean = require('gulp-clean');
+const clean = require('gulp-clean');
 const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+
 
 
 function build(cb) {
-  src('./app/assets/images/**/*.*')
-    .pipe(image({
-      pngquant: true,
-      optipng: true,
-      zopflipng: true,
-      jpegRecompress: true,
-      mozjpeg: true,
-      guetzli: true,
-      gifsicle: true,
-      svgo: true,
-      concurrent: 10,
-      quiet: false // defaults to false
-    }))
-    .pipe(dest('./app/assets/images/'));
-  cb();
+	src('./app/assets/images/**/**')
+		.pipe(
+			image({
+				pngquant: true,
+				optipng: true,
+				zopflipng: true,
+				jpegRecompress: true,
+				mozjpeg: true,
+				guetzli: true,
+				gifsicle: true,
+				svgo: true,
+				concurrent: 10,
+				quiet: false // defaults to false
+			})
+		)
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.mozjpeg({ quality: 100, progressive: true }),
+				imagemin.optipng({ optimizationLevel: 10 }),
+				imagemin.svgo({
+					plugins: [ { removeViewBox: true }, { cleanupIDs: true } ]
+				})
+			])
+		)
+		.pipe(dest('./app/assets/images/'));
+	cb();
 }
 
-
 function html(cb) {
-  src('./app/templates/**/*.html')
-    .pipe(html2pug( /* options for html2pug such as { fragment: true } */ ))
-    .pipe(dest('./app/templates/'));
-  cb();
+	src('./app/templates/**/*.html')
+		.pipe(html2pug(/* options for html2pug such as { fragment: true } */))
+		.pipe(dest('./app/templates/'));
+	cb();
 }
 
 function deleteHtml(cb) {
-  src('./app/templates/**/*.html')
-    .pipe(clean())
+	src('./app/templates/**/*.html').pipe(clean());
 
-  cb();
+	cb();
 }
-
 
 function webm(cb) {
-  src('./app/assets/images/**/*')
-
-  .pipe(webp({lossless: true
-  }))
-  .pipe(dest('./docs/assets/images/webp/'));
-
-  cb();
+	src('./app/assets/images/**/*')
+		.pipe(
+			webp({
+				lossless: true
+			})
+		)
+		.pipe(dest('./docs/assets/images/webp/'));
+	cb();
 }
-
-
 
 exports.webm = webm;
 exports.build = build;
