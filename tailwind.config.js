459d9df9b50f1657e2a2284ab3d0f7df89947917
Robert James Gabriel <robert_gabriel@outlook.com>
2021-10-29 16:53:40 +0100

    New pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
 		enabled: true
 	},
 	darkMode: false, // or 'media' or 'class'
-
+	theme: {
+		extend: {
+		  gridTemplateRows: {
+			'[auto,auto,1fr]': 'auto auto 1fr',
+		  },
+		},
+	  },
 	variants: {
 		extend: {}
 	},
