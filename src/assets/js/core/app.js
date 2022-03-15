d21eb0c55909eb252aa370a50c4e6d1e6b06270d
Robert James Gabriel <robert_gabriel@outlook.com>
2022-03-15 01:25:39 +0000

    k

---
+++ src/assets/js/core/app.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+
+function load() {
+	let isThere = document.getElementById('helperbird-website');
+	console.log(isThere);
+	new Vue({
+		el: '#helperbird-website',
+		data: {
+			menus: false,
+			mobileMenu: false,
+			productModal: false,
+		},
+	 mounted() {
+		console.log('mounted');
+		},
+		methods: {
+			handleMenu(menu) {
+				console.log(menu);
+				if (this.menus !== menu) {
+					this.menus = menu;
+				} else {
+					this.menus = null;
+				}
+			},
+
+			handleModal() {
+
+				this.productModal = !this.productModal;
+			},
+
+			handleMobileMenu() {
+
+				this.mobileMenu = !this.mobileMenu;
+			},
+
+		},
+		computed: {
+			showMenu: function () {
+				return this.menus;
+			},
+
+			showMobileMenu: function () {
+				return this.mobileMenu;
+			},
+
+			showModal: function () {
+				return this.productModal;
+			},
+		}
+	});
+}
+
+export { load }; // a list of exported variables
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,34 +1,13 @@
 // css
-import 'core-js/stable';
 import 'notyf/notyf.min.css'; // for React and Vue
 import 'regenerator-runtime/runtime';
 import 'tailwindcss/tailwind.css';
-import * as banner from './core/banner';
-import * as pricing_table from './core/extension-pricing';
-// Vue components
-import * as navigation from './core/navigation';
-import * as thirdParty from './core/third-party';
-import * as Youtube from './core/youtube';
+import * as app from './core/app';
 
 
-function load() {
-	setTimeout(() => {
 
-		Youtube.load();
-		// Vue components load
-		banner.load();
+app.load();
 
-		pricing_table.load();
-
-		thirdParty.crisp();
-		thirdParty.tagManager();
-		thirdParty.loadManager();
-		navigation.load();
-	}, 2000),
-		{
-			once: true
-		};
-}
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
@@ -39,4 +18,4 @@ if ('serviceWorker' in navigator) {
 	});
 }
 
-load();
+
