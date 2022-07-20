8e9c3c7c377ebb2d8f5baeac2e015ad7a97e5f25
Robert James Gabriel <robert_gabriel@outlook.com>
2022-07-20 23:14:32 +0100

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -1,88 +1,179 @@
-
 const lodash = require('lodash');
 const htmlmin = require('html-minifier');
 const svgContents = require('eleventy-plugin-svg-contents');
 const pluginPWA = require('./tools/eleventy-plugin-pwa');
+const fs = require('fs');
+const path = require('path');
 
+const { createCanvas, loadImage } = require('canvas');
+const { formatTitle } = require('./tools/format-title');
+const createSocialImageForArticle = (input, output) =>
+	new Promise((resolve, reject) => {
+		// read data from input file
+		const data = fs.readFileSync(input, {
+			encoding: 'utf-8'
+		});
+
+        const helperbirdLogo = './tools/images/helperbird-logo.png';
+		// get title from file data
+		const [, title] = data.match(/title:(.*)/);
+
+		const post = {
+			title: title,
+			author: 'Helperbird.com'
+		};
+
+		const width = 1200;
+		const height = 627;
+		// Set the coordinates for the image position.
+		const imagePosition = {
+			w: 75,
+			h: 75,
+			x: 475,
+			y: 75
+		};
+		// Because we are putting the image near the top (y: 75)
+		// move the title down.
+		const titleY = 300;
+		const titleLineHeight = 100;
+		// Bring up the author's Y value as well to make it all
+		// fit together nicely.
+		const authorY = 525;
+
+		const canvas = createCanvas(width, height);
+		const context = canvas.getContext('2d');
+
+		context.fillStyle = '#450a75';
+		context.fillRect(0, 0, width, height);
+
+		context.font = "bold 60pt 'PT Sans'";
+		context.textAlign = 'center';
+		context.fillStyle = '#fff';
+
+		const titleText = formatTitle(post.title);
+		context.fillText(titleText[0], 600, titleY);
+		if (titleText[1]) {
+			context.fillText(titleText[1], 600, titleY + titleLineHeight);
+		}
+
+		context.font = "40pt 'PT Sans'";
+		context.fillText(`${post.author}`, 600, authorY);
+
+		// Load the logo file and then render it on the screen.
+		loadImage(helperbirdLogo).then((image) => {
+			console.log(image);
+			const { w, h, x, y } = imagePosition;
+			context.drawImage(image, x, y, w, h);
+
+			const outputDir = path.dirname(output);
+			if (!fs.existsSync(outputDir)) {
+				fs.mkdirSync(outputDir, { recursive: true });
+			}
+
+			// write the output image
+
+			const stream = fs.createWriteStream(output);
+			stream.on('finish', resolve);
+			stream.on('error', reject);
+			canvas
+				.createJPEGStream({
+					quailty: 1.0
+				})
+				.pipe(stream);
+		});
+	});
 
 const manifest = {
-    'main.js': '/assets/js/main.bundle.js',
-    'main.css': '/assets/js/main.css'
+	'main.js': '/assets/js/main.bundle.js',
+	'main.css': '/assets/js/main.css'
 };
 const format = require('date-fns/format');
 const moment = require('moment');
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy({ 'src/assets/': '/assets/' });
+	eleventyConfig.addLiquidFilter('limit', (arr, limit) => arr.slice(0, limit));
+	eleventyConfig.addTransform('social-image', async function (content) {
+		// only handle blog posts
+		// if (!this.inputPath.endsWith('.md')) return content;
 
-    eleventyConfig.addPassthroughCopy({'src/assets/':  '/assets/'});
-    eleventyConfig.addLiquidFilter('limit', (arr, limit) => arr.slice(0, limit));
+		try {
+			await createSocialImageForArticle(
+				// our input article
+				this.inputPath,
 
+				// the output image name
+				this.outputPath.replace('.html', '.jpeg')
+			);
+		} catch (err) {
+			console.error(err);
+		}
 
+		// return normal content
+		return content;
+	});
 
-    eleventyConfig.addPlugin(pluginPWA, {
-        swDest: './docs/service-worker.js',
-        globDirectory: './docs'
-    });
-    eleventyConfig.addPlugin(svgContents);
+	eleventyConfig.addPlugin(pluginPWA, {
+		swDest: './docs/service-worker.js',
+		globDirectory: './docs'
+	});
+	eleventyConfig.addPlugin(svgContents);
 
-    // add `date` filter
-    eleventyConfig.addFilter('date', function (date, dateFormat) {
-        return format(date, dateFormat);
-    });
+	// add `date` filter
+	eleventyConfig.addFilter('date', function (date, dateFormat) {
+		return format(date, dateFormat);
+	});
 
-    // Random Filter: With the help from google search engine
-    eleventyConfig.addFilter('shuffle', (arr) => lodash.shuffle(arr));
+	// Random Filter: With the help from google search engine
+	eleventyConfig.addFilter('shuffle', (arr) => lodash.shuffle(arr));
 
-    eleventyConfig.addFilter('dateDisplay', function (input) {
+	eleventyConfig.addFilter('dateDisplay', function (input) {
+		let xx = moment(input).format('MMMM Do YYYY');
+		return xx;
+	});
 
-        let xx = moment(input).format('MMMM Do YYYY');
-        return xx;
-    });
+	let markdownIt = require('markdown-it');
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true
+	};
 
-    let markdownIt = require('markdown-it');
-    let options = {
-        html: true,
-        breaks: true,
-        linkify: true
-    };
+	eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+		// Eleventy 1.0+: use this.inputPath and this.outputPath instead
+		if (outputPath && outputPath.endsWith('.html')) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
 
-    eleventyConfig.addTransform('htmlmin', function (content, outputPath) { // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-        if (outputPath && outputPath.endsWith('.html')) {
-            let minified = htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
-                collapseWhitespace: true
-            });
-            return minified;
-        }
+		return content;
+	});
 
-        return content;
-    });
+	eleventyConfig.setLibrary('md', markdownIt(options));
+	eleventyConfig.setLiquidOptions({
+		dynamicPartials: false,
+		strictFilters: false // renamed from `strict_filters` in Eleventy 1.0
+	});
+	// Add a shortcode for bundled CSS.
+	eleventyConfig.addShortcode('bundledCss', function () {
+		return manifest['main.css'] ? `<link href="${manifest['main.css']}" rel="stylesheet" />` : '';
+	});
 
-    eleventyConfig.setLibrary('md', markdownIt(options));
-    eleventyConfig.setLiquidOptions({
-        dynamicPartials: false, strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
-    });
-    // Add a shortcode for bundled CSS.
-    eleventyConfig.addShortcode('bundledCss', function () {
-        return manifest['main.css'] ? `<link href="${
-            manifest['main.css']
-        }" rel="stylesheet" />` : '';
-    });
-
-    // Add a shortcode for bundled JS.
-    eleventyConfig.addShortcode('bundledJs', function () {
-        return manifest['main.js'] ? `<script src="${
-            manifest['main.js']
-        }"></script>` : '';
-    });
-    return {
-        markdownTemplateEngine: 'md',
-        dir: {
-            data: "../_data",
-            includes: "../_includes",
-            input: 'src/pages/',
-            output: 'docs'
-        }
-    };
+	// Add a shortcode for bundled JS.
+	eleventyConfig.addShortcode('bundledJs', function () {
+		return manifest['main.js'] ? `<script src="${manifest['main.js']}"></script>` : '';
+	});
+	return {
+		markdownTemplateEngine: 'md',
+		dir: {
+			data: '../_data',
+			includes: '../_includes',
+			input: 'src/pages/',
+			output: 'docs'
+		}
+	};
 };
