dc6312a4007a5170a3a5a40dc328084054c1a8ba
Robert James Gabriel <robert_gabriel@outlook.com>
2023-10-16 19:18:11 -0500

    Added extension page.

--- src/assets/js/core/app.js
+++ src/assets/js/core/app.js
@@ -2,8 +2,11 @@ import Typewriter from 'typewriter-effect/dist/core';
 import Vue from 'vue';
 import { crisp, loadManager, tagManager } from './third-party';
 import { youtube } from './youtube';
-function load() {
-	let isThere = document.getElementById('helperbird-website');
+
+const load = () => {
+	const isThere = document.getElementById('helperbird-website');
+
+	if (!isThere) return;
 
 	new Vue({
 		el: '#helperbird-website',
@@ -12,8 +15,6 @@ function load() {
 			mobileMenu: false,
 			productModal: false,
 			writer: null,
-
-			// Pricing Table
 			isYearly: true,
 			proLink: '',
 			studentLink: '',
@@ -22,65 +23,67 @@ function load() {
 			currency: 'usd',
 			docsMenu: false,
 			selectedTab: 0,
-			price: {
-				monthly: {
-					pro: '6.99',
-					proLink: '',
-					unlimited: '99.99',
-					unlimitedLink: ''
-				},
-				yearly: {
-					pro: '60.00',
-					proLink: '',
-					unlimited: '1500',
-					unlimitedLink: ''
-				},
-				student: '40',
-				currency: '$',
-				studentLink: ''
-			}
+			price: getDefaultPrice()
 		},
 		mounted() {
 			crisp();
 			tagManager();
 			loadManager();
-
 			youtube();
-			this.typer();
+			this.initializeTypewriters();
 		},
 		methods: {
-			switchType: function () {
+			switchType() {
 				this.isYearly = !this.isYearly;
 			},
-
-			handleModal: function () {
+			handleModal() {
 				this.openModal = !this.openModal;
 			},
+			initializeTypewriters() {
+				this.initializeTypewriter('typewriter', [
+					'Accessibility',
+					'Pdf Reader',
+					'Productivity',
+					'Reading support',
+					'Adapting the web',
+					'Dyslexia support',
+					'Text to speech',
+					'Voice typing',
+					'Word prediction',
+					"Reading Mode",
+					'Text extracting',
+					'Reading',
+					'Writing',
+					'Note taking',
+					'Speed reading',
+					'Tooltip reader',
+					'Color contrast adjuster', // added
+					'Magnifier tool', // added
 
-			typer() {
-				let id = 'typewriter';
-
-				let typerElement = document.getElementById(id);
-
+					'Screen reader friendly', // added
+					'Keyboard navigation', // added
+					'Customizable fonts', // added
+					'High contrast mode', // added
+					'Alt text support', // added
+					'Language translation' // added
+				]);
+				this.initializeTypewriter('typewriterProducts', [
+					'Google Chrome',
+					'Mozilla Firefox',
+					'Micosoft Edge',
+					'Google Docs',
+					'Google Slides',
+					'iPad',
+					'iOs',
+					'iPhone',
+					'Safari'
+				]);
+			},
+			initializeTypewriter(id, strings) {
+				const typerElement = document.getElementById(id);
 				if (typerElement) {
 					this.writer = new Typewriter(`#${id}`, {
-						strings: [
-							'Accessibility',
-							'Pdf Reader',
-							'Productivity',
-							'Reading support',
-							'Adapting the web',
-							'Dyslexia support',
-							'Text to speech',
-							'Voice typing',
-							'Word prediction',
-							'Text extracting',
-							'Reading',
-							'Writing',
-							'Note taking',
-							'Speed reading',
-							'Tooltip reader'
-						],
+						strings,
 						autoStart: true,
 						loop: true,
 						delay: 200
@@ -88,202 +91,180 @@ function load() {
 				}
 			},
 			handleMenu(menu) {
-				console.log(menu);
-				if (this.menus !== menu) {
-					this.menus = menu;
-				} else {
-					this.menus = null;
-				}
+				this.menus = this.menus !== menu ? menu : null;
 			},
-
-			handleModal() {
-				this.productModal = !this.productModal;
-			},
-
 			handleMobileMenu() {
 				this.mobileMenu = !this.mobileMenu;
 			}
 		},
 		computed: {
-			showYearly: function () {
+			showYearly() {
 				return this.isYearly;
 			},
+			priceDetails() {
+				return getPriceForCurrency(this.currency);
+			},
 			monthlyProPricing() {
-				var result = this.priceDetails.monthly.pro - this.priceDetails.monthly.pro / 3;
-				var resultWithOneDecimal = result.toFixed(2);
-				return resultWithOneDecimal;
+				return calculateDiscountedPrice(this.priceDetails.monthly.pro);
 			},
 			yearlyProPricing() {
-				var result = this.priceDetails.yearly.pro - this.priceDetails.yearly.pro / 3;
-				var resultWithOneDecimal = result.toFixed(2);
-				return resultWithOneDecimal;
+				return calculateDiscountedPrice(this.priceDetails.yearly.pro);
 			},
-
 			yearlyUnlimitedPricing() {
-				var result = this.priceDetails.yearly.unlimited - this.priceDetails.yearly.unlimited / 3;
-				var resultWithOneDecimal = result.toFixed(2);
-				return resultWithOneDecimal;
+				return calculateDiscountedPrice(this.priceDetails.yearly.unlimited);
 			},
 			monthlyUnlimitedPricing() {
-				var result = this.priceDetails.monthly.unlimited - this.priceDetails.monthly.unlimited / 3;
-				var resultWithOneDecimal = result.toFixed(2);
-				return resultWithOneDecimal;
+				return calculateDiscountedPrice(this.priceDetails.monthly.unlimited);
 			},
-			showModal: function () {
+			showModal() {
 				return this.openModal;
 			},
-
-			priceDetails: function () {
-				switch (this.currency) {
-					case 'aud': {
-						this.price.yearly.pro = '80.57';
-						this.price.yearly.proLink = 'https://buy.stripe.com/aEUaFl0uMfYM98I9AW';
-
-						this.price.yearly.unlimited = '1600';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/bIYbJp0uMaEs0CcaF2';
-
-						this.price.monthly.pro = '9.39';
-						this.price.monthly.proLink = 'https://buy.stripe.com/8wM4gXa5maEsbgQ9AV';
-
-						this.price.monthly.unlimited = '134.27';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/8wMcNtdhy8wk2Kk00n';
-
-						this.price.student = '53.73';
-						this.price.studentLink = 'https://buy.stripe.com/14kdRx6Ta27WdoYaF3';
-
-						this.price.currency = '$';
-
-						break;
-					}
-					case 'usd': {
-						this.price.yearly.pro = '60.00';
-						this.price.yearly.proLink = 'https://buy.stripe.com/4gw7t9gtK6oc98I8wJ';
-
-						this.price.yearly.unlimited = '1500';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/3cs8xda5m4g44Ss9AU';
-
-						this.price.monthly.pro = '6.99';
-						this.price.monthly.proLink = 'https://buy.stripe.com/9AQ14Ldhy13S4Ss5kw';
-
-						this.price.monthly.unlimited = '99.99';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/dR6cNt4L2dQEfx69AE';
-
-						this.price.student = '40.00';
-						this.price.studentLink = 'https://buy.stripe.com/14k5l16Ta9Ao1Gg7ss';
-
-						this.price.currency = '$';
-						break;
-					}
-					case 'cad': {
-						this.price.yearly.pro = '75.00';
-						this.price.yearly.proLink = 'https://buy.stripe.com/eVaeVBfpGh2Q3OocNd';
-
-						this.price.yearly.unlimited = '1500';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/28o8xdcdu6ocacM7sW';
-
-						this.price.monthly.pro = '8.75';
-						this.price.monthly.proLink = 'https://buy.stripe.com/bIY4gXb9qcMAet28wY';
-
-						this.price.monthly.unlimited = '125';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/9AQeVB4L2bIwacM7sV';
-
-						this.price.student = '50.00';
-						this.price.studentLink = 'https://buy.stripe.com/3cscNt3GY9AogBa4gG';
-
-						this.price.currency = '$';
-						break;
-					}
-					case 'euro': {
-						this.price.yearly.pro = '60.00';
-						this.price.yearly.proLink = 'https://buy.stripe.com/8wM9Bh4L2aEsacMfZv';
-
-						this.price.yearly.unlimited = '1200';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/4gw8xdgtK9Ao3Oo00z';
-
-						this.price.monthly.pro = '5.99';
-						this.price.monthly.proLink = 'https://buy.stripe.com/5kA5l17XefYMfx64gM';
-
-						this.price.monthly.unlimited = '99.99';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/8wM3cTcdubIwgBacNk';
-
-						this.price.student = '40.00';
-						this.price.studentLink = 'https://buy.stripe.com/eVa5l16Ta8wk2KkcNh';
-
-						this.price.currency = '€';
-						break;
-					}
-					case 'gbp': {
-						this.price.yearly.pro = '50.00';
-						this.price.yearly.proLink = 'https://buy.stripe.com/aEU9BhfpG5k8ckU5kW';
-
-						this.price.yearly.unlimited = '1500';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/5kA00H7XebIw84E7t6';
-
-						this.price.monthly.pro = '5.00';
-						this.price.monthly.proLink = 'https://buy.stripe.com/9AQ8xd6Ta13Set29Bb';
-
-						this.price.monthly.unlimited = '99.99';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/aEU6p5elC4g470A6p1';
-
-						this.price.student = '40.00';
-						this.price.studentLink = 'https://buy.stripe.com/14keVBfpGdQEacM7t2';
-
-						this.price.currency = '£';
-						break;
-					}
-					case 'nz': {
-						this.price.yearly.pro = '80.00';
-						this.price.yearly.proLink = 'https://buy.stripe.com/28o7t9a5m7sg3Oo7t8';
-
-						this.price.yearly.unlimited = '1700';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/9AQ7t9a5m27W84E6p7';
-
-						this.price.monthly.pro = '8.50';
-						this.price.monthly.proLink = 'https://buy.stripe.com/7sI6p591ifYM4SseVB';
-
-						this.price.monthly.unlimited = '145.00';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/6oE28P1yQeUI84EcNu';
-
-						this.price.student = '50.00';
-						this.price.studentLink = 'https://buy.stripe.com/cN2cNtgtKdQEbgQ3cR';
-
-						this.price.currency = '$';
-						break;
-					}
-					default:
-						this.price.yearly.pro = '60.00';
-						this.price.yearly.proLink = 'https://buy.stripe.com/4gw7t9gtK6oc98I8wJ';
-
-						this.price.yearly.unlimited = '1500';
-						this.price.yearly.unlimitedLink = 'https://buy.stripe.com/3cs8xda5m4g44Ss9AU';
-
-						this.price.monthly.pro = '6.99';
-						this.price.monthly.proLink = 'https://buy.stripe.com/9AQ14Ldhy13S4Ss5kw';
-
-						this.price.monthly.unlimited = '99.99';
-						this.price.monthly.unlimitedLink = 'https://buy.stripe.com/dR6cNt4L2dQEfx69AE';
-
-						this.price.student = '40.00';
-						this.price.studentLink = 'https://buy.stripe.com/14k5l16Ta9Ao1Gg7ss';
-
-						this.price.currency = '$';
-						break;
-				}
-				return this.price;
-			},
-			showMenu: function () {
+			showMenu() {
 				return this.menus;
 			},
-
-			showMobileMenu: function () {
+			showMobileMenu() {
 				return this.mobileMenu;
-			},
-
-			showModal: function () {
-				return this.productModal;
 			}
 		}
 	});
-}
+};
 
-export { load }; // a list of exported variables
+const calculateDiscountedPrice = (originalPrice) => {
+	const result = parseFloat(originalPrice) - parseFloat(originalPrice) / 3;
+	return result.toFixed(2);
+};
+
+const getDefaultPrice = () => {
+	return {
+		monthly: {
+			pro: '6.99',
+			proLink: '',
+			unlimited: '99.99',
+			unlimitedLink: ''
+		},
+		yearly: {
+			pro: '60.00',
+			proLink: '',
+			unlimited: '1500',
+			unlimitedLink: ''
+		},
+		student: '40',
+		currency: '$',
+		studentLink: ''
+	};
+};
+
+const getPriceForCurrency = (currency) => {
+	const prices = {
+		aud: {
+			yearly: {
+				pro: '80.57',
+				proLink: 'https://buy.stripe.com/aEUaFl0uMfYM98I9AW',
+				unlimited: '1600',
+				unlimitedLink: 'https://buy.stripe.com/bIYbJp0uMaEs0CcaF2'
+			},
+			monthly: {
+				pro: '9.39',
+				proLink: 'https://buy.stripe.com/8wM4gXa5maEsbgQ9AV',
+				unlimited: '134.27',
+				unlimitedLink: 'https://buy.stripe.com/8wMcNtdhy8wk2Kk00n'
+			},
+			student: '53.73',
+			currency: '$',
+			studentLink: 'https://buy.stripe.com/14kdRx6Ta27WdoYaF3'
+		},
+
+		usd: {
+			yearly: {
+				pro: '60.00',
+				proLink: 'https://buy.stripe.com/4gw7t9gtK6oc98I8wJ',
+
+				unlimited: '1500',
+				unlimitedLink: 'https://buy.stripe.com/3cs8xda5m4g44Ss9AU'
+			},
+			monthly: {
+				pro: '6.99',
+				proLink: 'https://buy.stripe.com/9AQ14Ldhy13S4Ss5kw',
+
+				unlimited: '99.99',
+				unlimitedLink: 'https://buy.stripe.com/dR6cNt4L2dQEfx69AE'
+			},
+			student: '40.00',
+			studentLink: 'https://buy.stripe.com/14k5l16Ta9Ao1Gg7ss',
+			currency: '$'
+		},
+
+		cad: {
+			yearly: {
+				pro: '75.00',
+				proLink: 'https://buy.stripe.com/eVaeVBfpGh2Q3OocNd',
+				unlimited: '1500',
+				unlimitedLink: 'https://buy.stripe.com/28o8xdcdu6ocacM7sW'
+			},
+			monthly: {
+				pro: '8.75',
+				proLink: 'https://buy.stripe.com/bIY4gXb9qcMAet28wY',
+				unlimited: '125',
+				unlimitedLink: 'https://buy.stripe.com/9AQeVB4L2bIwacM7sV'
+			},
+			student: '50.00',
+			studentLink: 'https://buy.stripe.com/3cscNt3GY9AogBa4gG',
+			currency: '$'
+		},
+		euro: {
+			yearly: {
+				pro: '60.00',
+				proLink: 'https://buy.stripe.com/8wM9Bh4L2aEsacMfZv',
+				unlimited: '1200',
+				unlimitedLink: 'https://buy.stripe.com/4gw8xdgtK9Ao3Oo00z'
+			},
+			monthly: {
+				pro: '5.99',
+				proLink: 'https://buy.stripe.com/5kA5l17XefYMfx64gM',
+				unlimited: '99.99',
+				unlimitedLink: 'https://buy.stripe.com/8wM3cTcdubIwgBacNk'
+			},
+			student: '40.00',
+			studentLink: 'https://buy.stripe.com/eVa5l16Ta8wk2KkcNh',
+			currency: '€'
+		},
+		gbp: {
+			yearly: {
+				pro: '50.00',
+				proLink: 'https://buy.stripe.com/aEU9BhfpG5k8ckU5kW',
+				unlimited: '1500',
+				unlimitedLink: 'https://buy.stripe.com/5kA00H7XebIw84E7t6'
+			},
+			monthly: {
+				pro: '5.00',
+				proLink: 'https://buy.stripe.com/9AQ8xd6Ta13Set29Bb',
+				unlimited: '99.99',
+				unlimitedLink: 'https://buy.stripe.com/aEU6p5elC4g470A6p1'
+			},
+			student: '40.00',
+			studentLink: 'https://buy.stripe.com/14keVBfpGdQEacM7t2',
+			currency: '£'
+		},
+		nz: {
+			yearly: {
+				pro: '80.00',
+				proLink: 'https://buy.stripe.com/28o7t9a5m7sg3Oo7t8',
+				unlimited: '1700',
+				unlimitedLink: 'https://buy.stripe.com/9AQ7t9a5m27W84E6p7'
+			},
+			monthly: {
+				pro: '8.50',
+				proLink: 'https://buy.stripe.com/7sI6p591ifYM4SseVB',
+				unlimited: '145.00',
+				unlimitedLink: 'https://buy.stripe.com/6oE28P1yQeUI84EcNu'
+			},
+			student: '50.00',
+			studentLink: 'https://buy.stripe.com/cN2cNtgtKdQEbgQ3cR',
+			currency: '$'
+		}
+	};
+
+	return prices[currency] || getDefaultPrice();
+};
+
+export { load };
