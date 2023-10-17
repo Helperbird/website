aa48eb6a565df484dd1b5f65277456545b57145d
Robert James Gabriel <robert_gabriel@outlook.com>
2023-10-17 00:53:20 -0500

    w

--- tools/images.js
+++ tools/images.js
@@ -1,23 +1,24 @@
 import imagemin from 'imagemin';
 import imageminJpegtran from 'imagemin-jpegtran';
 import imageminPngquant from 'imagemin-pngquant';
-
 import path from 'path';
 
-const assetsDirectory = './src/assets'; // Change this to your assets directory
-const outputDirectory = path.join(assetsDirectory, 'optimized');
+const assetsDirectory = './src/assets'; // Specify your assets directory
+const outputDirectory = assetsDirectory; // The optimized images will overwrite the original ones
 
 (async () => {
-	await imagemin([path.join(assetsDirectory, '**/*.{jpg,jpeg,png,svg}')], {
-		destination: outputDirectory,
-		plugins: [
-			imageminJpegtran(),
-			imageminPngquant({
-				quality: [0.6, 0.8] // Adjust quality as needed
-			})
-	
-		]
-	});
-
-	console.log('Images optimized and saved to', outputDirectory);
+	try {
+		await imagemin([path.join(assetsDirectory, '**/*.{jpg,jpeg,png,svg}')], {
+			destination: outputDirectory + '/',
+			plugins: [
+				imageminJpegtran(),
+				imageminPngquant({
+					quality: [0.6, 0.8] // Adjust quality as needed
+				})
+			]
+		});
+		console.log('Images optimized and saved back to their respective locations');
+	} catch (error) {
+		console.error('Error during image optimization:', error);
+	}
 })();
