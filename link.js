e0dd3ca6f0418282ab4d6662a184540b4ecf57ac
Robert James Gabriel <robert_gabriel@outlook.com>
2023-11-04 00:31:44 -0500

    Update code

--- link.js
+++ link.js
@@ -1,43 +1,34 @@
-
-
-
-
-const fs = require("fs");
-const path = require("path");
-const cheerio = require("cheerio");
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
 
 const directory = './src/pages/';
 
 const processFilesInDirectory = (directory) => {
-  fs.readdirSync(directory).forEach((filename) => {
-    const filepath = path.join(directory, filename);
-    const stats = fs.statSync(filepath);
+	fs.readdirSync(directory).forEach((filename) => {
+		const filepath = path.join(directory, filename);
+		const stats = fs.statSync(filepath);
 
-    // If it is a directory, recursively go through it
-    if (stats.isDirectory()) {
-      return processFilesInDirectory(filepath);
-    }
+		// If it is a directory, recursively go through it
+		if (stats.isDirectory()) {
+			return processFilesInDirectory(filepath);
+		}
 
-    // Otherwise, if it's a file ending in .vue, process it
-    if (filename.endsWith(".liquid") || filename.endsWith(".html")) {
-      const htmlContent = fs.readFileSync(filepath, "utf-8");
-      const $ = cheerio.load(htmlContent);
-      const imgTags = $("a");
+		// Otherwise, if it's a file ending in .vue, process it
+		if (filename.endsWith('.liquid') || filename.endsWith('.html')) {
+			const htmlContent = fs.readFileSync(filepath, 'utf-8');
+			const $ = cheerio.load(htmlContent);
+			const imgTags = $('a');
 
-      imgTags.each((i, imgTag) => {
-        const alt = $(imgTag).attr("title");
+			imgTags.each((i, imgTag) => {
+				const alt = $(imgTag).attr('title');
 
-    
-        if (!alt) {
-          console.log(
-            `${filename}: Missing title tag in link ${i + 1}: ${$(
-              imgTag
-            ).toString()}`
-          );
-        }
-      });
-    }
-  });
+				if (!alt) {
+					console.log(`${filename}: Missing title tag in link ${i + 1}: ${$(imgTag).toString()}`);
+				}
+			});
+		}
+	});
 };
 
 processFilesInDirectory(directory);
